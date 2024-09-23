a824845803c38af7e93cfd721f3f28489672f7b7
istiak19 <istiak15-14128@diu.edu.bd>
2024-09-23 12:19:41 +0600

    donation-history button commit

---
+++ js/features.js
@@ -0,0 +1,29 @@
+// History Button Click
+
+document.getElementById('btn-history').addEventListener('click', function () {
+    // History Button
+    const historyButton = document.getElementById('btn-history')
+    historyButton.classList.remove('text-gray-500', 'border', 'border-gray-400')
+    historyButton.classList.add('bg-[#B4F461]', 'text-black');
+    document.getElementById('donation-container').classList.add('hidden')
+
+    // Donation Button
+    const donationButton = document.getElementById('btn-donation');
+    donationButton.classList.remove('bg-[#B4F461]', 'text-black');
+    donationButton.classList.add('text-gray-500', 'border', 'border-gray-400')
+})
+
+// Donation Button Click
+
+document.getElementById('btn-donation').addEventListener('click', function () {
+    // Donation Button
+    const donationButton = document.getElementById('btn-donation');
+    donationButton.classList.add('bg-[#B4F461]', 'text-black')
+    donationButton.classList.remove('text-gray-500', 'border', 'border-gray-400')
+    document.getElementById('donation-container').classList.remove('hidden')
+
+    // History Button
+    const historyButton = document.getElementById('btn-history');
+    historyButton.classList.remove('bg-[#B4F461]', 'text-black');
+    historyButton.classList.add('text-gray-500', 'border', 'border-gray-400')
+})
